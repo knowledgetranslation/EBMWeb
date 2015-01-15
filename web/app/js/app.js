874453e3064e6f3248ca254db3d9a5cbf518f96a
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2015-01-15 10:54:17 -0500

    Master/sub-views

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -1,44 +1,4 @@
 var sc        = new app.models.StatsCalculator();
 var scv       = new app.views.StatsCalculatorView({model: sc});
 
-$("#statsCalculator").html(scv.el);
-
-
-var dt        = new app.models.DiagnosticTest();
-
-var dtv       = new app.views.DiagnosticTestView({model: dt});
-var dtfv      = new app.views.DiagnosticTestFormView({model: dt});
-var dtlrfv    = new app.views.DiagnosticTestLRFormView({model: dt});
-var dtrv      = new app.views.DiagnosticTestResultsView({model: dt});
-
-$("#diagnosticTest").html(dtv.el);
-$("#diagnosticTestForm").html(dtfv.el);
-$("#diagnosticTestLRForm").html(dtlrfv.el);
-$("#diagnosticTestResults").html(dtrv.el);
-
-
-var ps        = new app.models.ProspectiveStudy();
-
-var psv       = new app.views.ProspectiveStudyFormView({model: ps});
-var psrv      = new app.views.ProspectiveStudyResultsView({model: ps});
-
-$("#prospectiveStudy").html(psv.el);
-$("#prospectiveStudyResults").html(psrv.el);
-
-
-var ccs        = new app.models.CaseControlStudy();
-
-var ccsv       = new app.views.CaseControlStudyView({model: ccs});
-var ccsrv      = new app.views.CaseControlStudyResultsView({model: ccs});
-
-$("#caseControlStudy").html(ccsv.el);
-$("#caseControlStudyResults").html(ccsrv.el);
-
-
-var rct        = new app.models.RandomizedControlTrial();
-
-var rctv       = new app.views.RandomizedControlTrialView({model: rct});
-var rctrv      = new app.views.RandomizedControlTrialResultsView({model: rct});
-
-$("#rctStudy").html(rctv.el);
-$("#rctStudyResults").html(rctrv.el);
+$("#main").html(scv.el);
--- web/app/js/views/statsCalculator.js
+++ web/app/js/views/statsCalculator.js
@@ -12,10 +12,30 @@ app.views.StatsCalculatorView = Backbone.View.extend({
   },
 
   initialize: function() {
+    this.subviews = {};
     this.render();
+
+    var dt        = new app.models.DiagnosticTest();
+    var ps        = new app.models.ProspectiveStudy();
+    var ccs        = new app.models.CaseControlStudy();
+    var rct        = new app.models.RandomizedControlTrial();
+
+    this.subviews.dtv       = new app.views.DiagnosticTestView({model: dt, el: this.$("#diagnosticTest")});
+    this.subviews.dtfv      = new app.views.DiagnosticTestFormView({model: dt, el: this.$("#diagnosticTestForm")});
+    this.subviews.dtlrfv    = new app.views.DiagnosticTestLRFormView({model: dt, el: this.$("#diagnosticTestLRForm")});
+    this.subviews.dtrv      = new app.views.DiagnosticTestResultsView({model: dt, el: this.$("#diagnosticTestResults")});
+
+    this.subviews.psv       = new app.views.ProspectiveStudyFormView({model: ps, el: this.$("#prospectiveStudy")});
+    this.subviews.psrv      = new app.views.ProspectiveStudyResultsView({model: ps, el: this.$("#prospectiveStudyResults")});
+
+    this.subviews.csv       = new app.views.CaseControlStudyView({model: ccs, el: this.$("#caseControlStudy")});
+    this.subviews.ccsrv      = new app.views.CaseControlStudyResultsView({model: ccs, el: this.$("#caseControlStudyResults")});
+
+    this.subviews.rctv       = new app.views.RandomizedControlTrialView({model: rct, el: this.$("#rctStudy")});
+    this.subviews.rctrv      = new app.views.RandomizedControlTrialResultsView({model: rct, el: this.$("#rctStudyResults")});
   },
 
   render: function() {
-    this.el.innerHTML = this.template();
+    this.$el.html(this.template());
   }
 });
